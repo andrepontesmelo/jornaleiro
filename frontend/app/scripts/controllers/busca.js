83a0326c1a746aa9c0fa4cce6344288fbfd2822a
André Pontes <andrepontes@gmail.com>
2016-01-15 19:47:51 -0200

    AngularJS: Destacando texto procurado.

--- frontend/app/scripts/controllers/busca.js
+++ frontend/app/scripts/controllers/busca.js
@@ -1,10 +1,25 @@
 'use strict';
 
 
-angular.module('frontendApp')
-  .controller('BuscaCtrl', ['$scope', 'buscaFactory', function ($scope, buscaFactory) {
 
-    buscaFactory.get({busca: "de_macedo_e_melo"}, function (buscaFactory) {
-      $scope.resultadoBusca = buscaFactory.resultadoBusca;
-    });
-  }]);
+angular.module('frontendApp')
+  .controller('BuscaCtrl', ['$scope', '$sce', 'buscaFactory',  function ($scope, $sce, buscaFactory) {
+
+    $scope.change = function(){
+        buscaFactory.get({busca: $scope.query.replace(/ /g, '_')}, function (buscaFactory) {
+
+          $scope.resultadoBusca = buscaFactory.resultadoBusca;
+        });
+
+    $scope.highlight = function(text, search) {
+
+      if (!search) {
+        return $sce.trustAsHtml(text);
+      }
+
+      return $sce.trustAsHtml(unescape(escape(text).replace(new RegExp(escape(search), 'gi'), '<span class="highlightedText">$&</span>')));
+    };
+
+  };
+
+}]);

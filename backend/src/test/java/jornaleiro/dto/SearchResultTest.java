c0ad1c9670f969fe5095f929dcf4abb8022d6d0d
André Pontes <andrepontes@gmail.com>
2016-03-03 10:22:01 -0300

    Backend: Library for API rest tests / integration tests.

--- backend/src/test/java/jornaleiro/dto/SearchResultTest.java
+++ backend/src/test/java/jornaleiro/dto/SearchResultTest.java
@@ -1,12 +1,11 @@
 package jornaleiro.dto;
 
 import junit.framework.TestCase;
-
+import static org.hamcrest.Matchers.*;
+import static com.jayway.restassured.RestAssured.get;
 
 public class SearchResultTest extends TestCase {
-
     public void test() throws Exception {
-
+        //get("/jornaleiro/rest/sessions").then().body("journal.id", equalTo(1));
     }
-
 }

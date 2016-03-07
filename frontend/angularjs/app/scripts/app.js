0bb9fbc1ff5074bf502485088dfd0f6124ecb318
André Pontes <andrepontes@gmail.com>
2016-03-07 09:42:33 -0300

    AngularJS: Redirecting to search @ initial page.

--- frontend/angularjs/app/scripts/app.js
+++ frontend/angularjs/app/scripts/app.js
@@ -20,21 +20,10 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/busca', {
         templateUrl: 'views/search.html',
         controller: 'SearchCtrl',
         controllerAs: 'search'
-      })
-      .when('/documento/:id', {
+      }).when('/documento/:id', {
         templateUrl: 'views/document.html',
         controller: 'DocumentCtrl',
         controllerAs: 'document'
